4c43ac200188c25fa9622fd9ba26e711d8352047
Ayesha-Rathnayake <136638470+Ayesha-Rathnayake@users.noreply.github.com>
2025-11-02 13:12:56 +0530

    Revert Admin

--- backend/authentication-service/src/main/java/com/servexa/auth/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.servexa.auth.repository;
-
-import com.servexa.auth.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
