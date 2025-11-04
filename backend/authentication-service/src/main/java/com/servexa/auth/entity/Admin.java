b06fe506d2d4f7b78e88afbb2d9698afdf77b4a2
OshanIndunil <155097333+OshanIndunil@users.noreply.github.com>
2025-11-04 11:10:14 +0530

    dev1

--- backend/authentication-service/src/main/java/com/servexa/auth/entity/Admin.java
+++ backend/authentication-service/src/main/java/com/servexa/auth/entity/Admin.java
@@ -9,7 +9,8 @@ import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import jakarta.persistence.column;
+import jakarta.persistence.Column;
+
 
 @Entity
 @Table(name="admins")
